c742939fdd342b50bbf913f2d9c785f9f39d1289
seokyoung-kim <seokyoungkimm@gmail.com>
2020-06-16 18:10:42 +0900

    first commit

---
+++ src/main/java/com/sundayspringstudy/planweaver/PlanWeaverApplication.java
@@ -0,0 +1,13 @@
+package com.sundayspringstudy.planweaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanWeaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanWeaverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sundayspringstudy/planweaver/PlanWeaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sundayspringstudy.planweaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanWeaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
